a5b8942aba67edf485a2b1695e3fdd0e14ad924c
Alexandre NICOLAIE <alexandre.nicolaie@gmail.com>
2018-11-10 18:30:41 +0100

    Refactor SocketIO

---
+++ Assets/Scripts/Network/WebSocketPacket.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
+
+namespace Network
+{
+    public class WebSocketPacket
+    {
+        [JsonProperty("type")] public string Type;
+        [JsonProperty("link_id")] public string LinkId;
+        [JsonProperty("body")] public Dictionary<string, string> Body;
+
+        public static implicit operator JObject(WebSocketPacket ws) => JObject.FromObject(ws);
+    }
+}
