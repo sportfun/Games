fe318ed9075a7b994e6c2dd6143e62b7c922ccd7
Alexandre NICOLAIE <alexandre.nicolaie@gmail.com>
2018-11-12 23:21:45 +0100

    Fix NullException in InputManager

--- Assets/Scripts/Input/InputManager.cs
+++ Assets/Scripts/Input/InputManager.cs
@@ -14,14 +14,14 @@ public class InputManager : MonoBehaviour
     [SerializeField] private int _leftValue = 0;
     [SerializeField] private int _rightValue = 1;
     [Space(5)]
-    [SerializeField] private InputEvent _leftInputEvent;
-    [SerializeField] private InputEvent _rightInputEvent;
+    [SerializeField] private InputEvent _leftInputEvent = new InputEvent();
+    [SerializeField] private InputEvent _rightInputEvent = new InputEvent();
     
     [Header("Speed")]
     [SerializeField] private FloatVariable _minSpeed;
     [SerializeField] private FloatVariable _maxSpeed;
     [Space(5)]
-    [SerializeField] private SpeedEvent _speedChangeEvent;
+    [SerializeField] private SpeedEvent _speedChangeEvent = new SpeedEvent();
 
     #endregion
 
